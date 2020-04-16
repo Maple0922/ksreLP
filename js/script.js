cb6372c3d82ddc69899f4a206325edf3b0df02c5
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-04-16 19:56:00 +0900

    2020/04/16 19:56

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 $(function(){
+  // smooth scroll
+
+  $('a[href^=#]').click(function() {
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      $('.hamburger').removeClass('hamburger--active');
+      $('.hamburger-menu').stop().fadeOut(200);
+      return false;
+   });
+
   // first fade in
 
   $('.eyecatch, .title, .sub-title, .hamburger').hide();
